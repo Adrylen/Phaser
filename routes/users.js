9e286c72a719481b8ddc661d52743aebd36aac2e
ArthurT <arthur.talpaert@isen-lille.fr>
2016-01-27 15:28:44 +0100

    creation route users + vue

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('users/add');
+});
+
+router.get('/add', function(req, res, next){
+  res.render('users/add', { title : 'Rentre dans nos rang!'});
+})
+
+router.post('/addSubmit', function(reg, res, next){
+  res.redirect('all');
+})
+
+router.get('/login', function(req, res, next){
+
+})
+
+router.get('/all', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+        res.render('users/all', {
+                "userlist" : docs
+            });
+       });
 });
 
 module.exports = router;
