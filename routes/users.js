89c0a0f6c22fa461668eff63b720d7dfbb4938d1
ArthurT <arthur.talpaert@isen-lille.fr>
2016-01-29 18:46:19 +0100

    ajout du package de test mocha et creation d'un model user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var User = require('./models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.redirect('users/add');
+
+  // tuto beginning
+
+  // create a new user called chris
+  var chris = new User({
+    name: 'Chris',
+    username: 'sevilayha',
+    password: 'password'
+  });
+
+  // call the custom method. this will just add -dude to his name
+  // user will now be Chris-dude
+  chris.dudify(function(err, name) {
+    if (err) throw err;
+
+    console.log('Your new name is ' + name);
+  });
+
+  // call the built-in save method to save to the database
+  chris.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully!');
+  });
+
+  // tuto end
+
 });
 
 router.get('/add', function(req, res, next){
