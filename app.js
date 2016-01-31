51097d820b6c0d7fca5c1c757da97ba3c79554cd
Adrylen <adrien.martinez@isen-lille.fr>
2016-01-31 23:19:41 +0100

    Ajout d'un favicon.ico Commentaires sur des variables empêchant le lancement de npm

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var flash = require('connect-flash');
+//var Strategy = require('passport-local').Strategy;
+//var flash = require('connect-flash');
 var morgan = require('morgan');
-var session = require('express-session');
+//var session = require('express-session');
 
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
