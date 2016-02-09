22a98342f142069039f21d75ae25233e409376db
ArthurT <arthur.talpaert@isen-lille.fr>
2016-02-09 19:36:31 +0100

    .gitignore is now working

--- routes/tmp.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {
-	res.render('tmp/isometric', {title : 'Essais Isometrics !'});
-});
-
-module.exports = router;
