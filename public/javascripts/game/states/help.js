0036174db07528856dacc608c7e5b024ccfd79f3
ArthurT <arthur.talpaert@isen-lille.fr>
2016-02-09 19:53:16 +0100

    commit init

---
+++ public/javascripts/game/states/help.js
@@ -0,0 +1,24 @@
+var help = function(game) {};
+
+help.prototype.init = function()
+{
+
+};
+
+help.prototype.preload = function()
+{
+	game.load.spritesheet('back', 'images/backButton.png', 204, 160);
+};
+
+help.prototype.create = function()
+{
+	back = game.add.button(game.world.width - 250, game.world.height - 100, 'back', null, this, 1, 0, 2);
+	back.width = 120;
+	back.height = 50;
+	back.onInputUp.add(this.back, this);
+};
+
+help.prototype.back = function()
+{
+	game.state.start('menu');
+};
---
+++ routes/users.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
+
+            /* GET */
+
+router.get('/', function(req, res, next) {
+  res.redirect('users/add');
+});
+router.get('/add', function(req, res, next){
+  res.render('users/add', { title : 'Rentre dans nos rangs!'});
+})
+router.get('/login', function(req, res, next){
+  res.render('users/login', { title : 'Reviens coloniser la galaxie!'});
+})
+router.get('/all', function(req, res) {
+  // get all the users
+  User.find({
+
+  }, function(err, users) {
+    if (err) throw err;
+    res.render('users/all', {users : users});
+  });
+});
+router.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+router.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.redirect('all');
+  })
+
+          /* POST */
+
+router.post('/add', function(req, res){
+  bcrypt.hash(req.body.password, 8, function(err, hash) {
+    var newUser = new User({
+      username: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) throw err;
+    });
+  });
+  res.redirect('all');
+})
+
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: 'login' }),
+  function(req, res) {
+    res.redirect('all');
+  });
+
+
+
+module.exports = router;
