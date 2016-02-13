be3dc39a6319a52466af785bda86f4d7af0b02e4
ArthurT <arthur.talpaert@isen-lille.fr>
2016-02-13 18:55:28 +0100

    structure users ajout planet, ship, solar system, ressources etc...

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ var userSchema = new Schema({
   created_at: Date,
   updated_at: Date,
   planets: Array,
-  solar_system: Number
+  solar_system: String
 });
 
 // custom method to add string to end of name
