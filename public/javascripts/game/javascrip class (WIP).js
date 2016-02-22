570c7963edeb2a7409c0042cb6dbc6033f978c37
digif <digifi@laposte.net>
2016-02-22 08:05:11 +0100

    début d'écriture des classes du jeu

---
+++ public/javascripts/game/javascrip class (WIP).js
@@ -0,0 +1,20 @@
+// http://www.phpied.com/3-ways-to-define-a-javascript-class/
+
+/*
+function Apple (type) {
+    this.type = type;
+    this.color = "red";
+}
+
+Apple.prototype.getInfo = function() {
+    return this.color + ' ' + this.type + ' apple';
+};
+
+var apple = {
+    type: "macintosh",
+    color: "red",
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+}
+*/
