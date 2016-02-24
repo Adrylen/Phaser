97b2eb6bc2801508450612ca7db8a4ba24e6872a
Adrylen <adrien.martinez@isen-lille.fr>
2016-02-24 11:49:00 +0100

    Tests d'insertion des patchs Pure-Data (success)

--- public/javascripts/webpd/examples.js
+++ public/javascripts/webpd/examples.js
@@ -1,8 +1,8 @@
 webPdExamples = {
-  
+
   init: function() {
     // starting in iOS9, audio will only be unmuted if the context is created on "touchend".
-    // see : https://github.com/sebpiq/WebPd/issues/81  
+    // see : https://github.com/sebpiq/WebPd/issues/81
     var is_iOS = /iPad|iPhone|iPod/.test(navigator.platform)
       , eventType = is_iOS ? 'touchend' : 'click'
 
