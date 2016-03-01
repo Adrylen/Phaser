7c954b87abbc14b219e5c3c6e50d3959e0f331b8
Adrylen <adrien.martinez@isen-lille.fr>
2016-03-01 22:45:05 +0100

    Premiers tests WebPd

--- public/javascripts/webpd/test.js
+++ public/javascripts/webpd/test.js
@@ -4,3 +4,14 @@ $.get('../patches/test.pd', function(patchStr) {
 	patch = Pd.loadPatch(patchStr);
 	Pd.start();
 });
+
+$("#mute").click( function() {
+	if($("#music").text() == "On") {
+		$("#music").text("Off");
+		Pd.send('sound', [parseFloat(0)]);
+	}
+	else {
+		$("#music").text("On");
+		Pd.send('sound', [parseFloat(1)]);
+	}
+});
