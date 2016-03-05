65164123fe1d3fab4668e69dcdf4c8512de07d98
digif <digifi@laposte.net>
2016-03-05 19:14:10 +0100

    planets are now declared in an array the number of planets is changed by changing the value of : nb_of_player

--- public/javascripts/game/states/planets.js
+++ public/javascripts/game/states/planets.js
@@ -24,6 +24,12 @@ Cette relation se traduit aussi par la fameuse troisième
 
 var theta = 1;
 
+//*************************
+var planet = [];
+var values = [1.00, 0.84, 0.70, 0.55, 0.39, 0.23];
+var nb_of_player = 6;
+//*************************
+
 console.log(JSON.stringify(solar_system,null, 4));	// you're no able to use the object solar_system
 
 Planets.prototype.init = function () {
@@ -53,12 +59,25 @@ Planets.prototype.create = function () {
 	game.add.image(0, 30, 'galaxy');
 
 	sun = game.add.image(game.width/2-35, (game.height-30)/2-5, 'sun'); sun.width = 70; sun.height = 70;
+
+	/*
 	planet1 = game.add.image(0, 0, 'planet1'); planet1.width = 32; planet1.height = 32;
 	planet2 = game.add.image(0, 0, 'planet2'); planet2.width = 32; planet2.height = 32;
 	planet3 = game.add.image(0, 0, 'planet3'); planet3.width = 32; planet3.height = 32;
 	planet4 = game.add.image(0, 0, 'planet4'); planet4.width = 32; planet4.height = 32;
 	planet5 = game.add.image(0, 0, 'planet5'); planet5.width = 32; planet5.height = 32;
 	planet6 = game.add.image(0, 0, 'planet6'); planet6.width = 32; planet6.height = 32;
+	*/
+
+	//*************************
+	for (var i = 0; i < nb_of_player; i++) {
+		var name = 'planet'.concat((i+1).toString());
+		console.log(name);
+		planet[i] = game.add.image(0, 0, name);
+		planet[i].width = 32;
+		planet[i].height = 32;
+	}
+	//*************************
 
 	topBar = game.add.image(0, 0, 'topBar'); topBar.height = 30;
 	coin = game.add.image(3, 2, 'coin'); coin.width = 26; coin.height = 26;
@@ -71,12 +90,22 @@ Planets.prototype.update = function () {
 	//	demi grand axe de l'ellipse
 	var a = 470, b = 220;
 	//	vient de l'équation d'une ellipse
+	/*
 	planet1.x = this.moveX(planet1.width, a*1.00, (theta+1)*0.23); planet1.y = this.moveY(planet1.height, b*1.00, (theta+1)*0.23);
 	planet2.x = this.moveX(planet2.width, a*0.84, (theta-1.3)*0.39); planet2.y = this.moveY(planet2.height, b*0.84, (theta-1.3)*0.39);
 	planet3.x = this.moveX(planet3.width, a*0.70, (theta+1.6)*0.55); planet3.y = this.moveY(planet3.height, b*0.70, (theta+1.6)*0.55);
 	planet4.x = this.moveX(planet4.width, a*0.55, (theta-1.9)*0.70); planet4.y = this.moveY(planet4.height, b*0.55, (theta-1.9)*0.70);
 	planet5.x = this.moveX(planet5.width, a*0.39, (theta+2.2)*0.84); planet5.y = this.moveY(planet5.height, b*0.39, (theta+2.2)*0.84);
 	planet6.x = this.moveX(planet6.width, a*0.23, (theta-2.5)*1.00); planet6.y = this.moveY(planet6.height, b*0.23, (theta-2.5)*1.00);
+	*/
+
+	//*************************
+	for (var i = 0; i < planet.length; i++) {
+	planet[i].x = this.moveX(planet[i].width, a*values[i], (theta+1)*values[5-i]);
+	planet[i].y = this.moveY(planet[i].height, b*values[i], (theta+1)*values[5-i]);
+	}
+	//*************************
+
 };
 
 Planets.prototype.render = function () {
