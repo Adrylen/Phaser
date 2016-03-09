7395846e68de651faca23cf7fb640cd395a468b9
Adrylen <adrien.martinez@isen-lille.fr>
2016-03-09 15:52:57 +0100

    Ajout des boutons manquants (Add - Login) Tentative : topbar dynamique

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ io.on('connection', function(socket, req){
       socket.broadcast.emit('user disconnected');
     })
 
-    var maxPlayer = 2;
+    var maxPlayer = 6;
     if(usernames.length == maxPlayer) {
       socket.emit('start ready');
       socket.broadcast.emit('start ready');
