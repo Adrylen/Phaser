c2c15bc9d6366a25c94c5b56c77456adc45ae65c
ArthurT <arthur.talpaert@isen-lille.fr>
2016-03-30 17:02:10 +0200

    fonction update pour adrien

--- public/javascripts/game/window/upgrade.js
+++ public/javascripts/game/window/upgrade.js
@@ -9,8 +9,18 @@ Upgrade.prototype = {
     },
 
     //Update server
-    update: function() {
+    /* Attention ask_for et against sont des objets de la forme:
+    *  var o = { ressource : nom_de_la_ressource, quantity: quantite };
+    */
+    update: function(from_user_id, ask_for, to_user_id, against) {
         /* ARTHUR */
+        var data = {
+          from_user_id = from_user_id,
+          ask_for = ask_for,
+          to_user_id = to_user_id,
+          against = against
+        }
+        socket.emit('AskCommerce', data);
     }
 };
 
