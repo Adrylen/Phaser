9db0c847b293df9aa53206de34d49e2b4522a8b9
digif <digifi@laposte.net>
2016-04-20 14:33:34 +0200

    tous les machins technique sur commerce

--- public/javascripts/game/states/commerce.js
+++ public/javascripts/game/states/commerce.js
@@ -1,19 +1,40 @@
 var Commerce = function(game) {};
 
-Commerce.prototype = function () {
+var from_user_id;
+var ask_for = {ressource : "", amount : 0};
+var to_user_id;
+var against = {ressource : "", amount : 0};
 
 
-	/* Attention "ask_for" et "against" sont des objets de la forme:
-	*  var o = { ressource : nom_de_la_ressource, amount: quantite };
+Commerce.prototype.create = function() {
+
+	game.stage.backgroundColor = '#182d3b';
+
+	text = game.add.text(32, 0, player.ressources.kaga, {font: "bold 26px Century Schoolbook L", fill: "#f19010"});
+	text.height = 33;
+	confirmer = game.add.button(game.world.centerX - 95, 400, confirmed, this);
+	//button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
+
+};
+
+
+function confirmed() {
+	/* ARTHUR */
+	/*
+	Attention "ask_for" et "against" sont des objets de la forme:
+	var o = { ressource : nom_de_la_ressource, amount: quantite };
 	*/
-	update: function(from_user_id, ask_for, to_user_id, against) {
-			/* ARTHUR */
-			var data = {
-				from_user_id = from_user_id,
-				ask_for = ask_for,
-				to_user_id = to_user_id,
-				against = against
-			}
-			socket.emit('AskCommerce', data);
-	}
+
+	var data = {
+		from_user_id : from_user_id,
+		ask_for : ask_for,
+		to_user_id : to_user_id,
+		against : against
+	};
+	socket.emit('AskCommerce', data);
+}
+
+Planets.prototype.update = function () {
+	text.text = player.ressources.kaga;
+	text.visible = false;
 };
