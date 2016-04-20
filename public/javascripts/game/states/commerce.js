0b4cde227f8fe0e8ece4cb71721d72a16a0f3a0a
digif <digifi@laposte.net>
2016-04-20 15:06:34 +0200

    mise en place du commerce (bug?)

--- public/javascripts/game/states/commerce.js
+++ public/javascripts/game/states/commerce.js
@@ -7,13 +7,12 @@ var against = {ressource : "", amount : 0};
 
 
 Commerce.prototype.create = function() {
-
 	game.stage.backgroundColor = '#182d3b';
 
-	text = game.add.text(32, 0, player.ressources.kaga, {font: "bold 26px Century Schoolbook L", fill: "#f19010"});
-	text.height = 33;
-	confirmer = game.add.button(game.world.centerX - 95, 400, confirmed, this);
-	//button = game.add.button(game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
+	bouh = game.add.text(32, 0, player.ressources.kaga, {font: "bold 26px Century Schoolbook L", fill: "#f19010"});
+	bouh.height = 33;
+	//confirmer = game.add.button(game.world.centerX - 95, 400, confirmed, this);
+	//button = game.add.button(game.world.centerX - 95, 400, 'button', confirmed, this, 2, 1, 0);
 
 };
 
@@ -24,17 +23,17 @@ function confirmed() {
 	Attention "ask_for" et "against" sont des objets de la forme:
 	var o = { ressource : nom_de_la_ressource, amount: quantite };
 	*/
-
+	console.log("yolo!");
 	var data = {
 		from_user_id : from_user_id,
 		ask_for : ask_for,
 		to_user_id : to_user_id,
 		against : against
 	};
-	socket.emit('AskCommerce', data);
+	//socket.emit('AskCommerce', data);
 }
 
 Planets.prototype.update = function () {
-	text.text = player.ressources.kaga;
-	text.visible = false;
+	bouh.text = player.ressources.kaga;
+	bouh.visible = false;
 };
