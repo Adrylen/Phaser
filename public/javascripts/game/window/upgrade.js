11d2f6a0e28520e7c1f1e890bfaba69eae460830
Adrylen <adrien.martinez@isen-lille.fr>
2016-04-27 16:01:04 +0200

    Upgrade buildings

--- public/javascripts/game/window/upgrade.js
+++ public/javascripts/game/window/upgrade.js
@@ -9,37 +9,46 @@ Upgrade.prototype = {
     // Display
     preload: function() {
         game.load.image('quit', '../images/buttons/quit.png');
+        game.load.image('upgrade_coin', '../images/assets/coin.png');
     },
 
     create: function() {
+        /* Quit Cross */
         cross = game.add.sprite(1000, 45, 'quit');
         cross.inputEnabled=true;
         cross.events.onInputDown.add(function () {
             cadre.visible = false;
             cross.visible = false;
+            upgrade_coin.visible = false;
             popUp = false;
         }, this);
         cross.visible = false;
 
+        /* Pop Up */
         cadre = game.add.graphics(45, 25);
+        cadre.visible = false;
 
         cadre.beginFill(0x061452, 0.7);
-
         cadre.moveTo(0,25);
         cadre.lineTo(1000,25);
         cadre.lineTo(1000,600);
         cadre.lineTo(0,600);
         cadre.lineTo(0,25);
-
         cadre.endFill();
 
-        cadre.visible = false;
+        /* Upgrade Kaga */
+        upgrade_coin = game.add.sprite(100, 100, 'upgrade_coin');
+        upgrade_coin.inputEnabled=true;
+        upgrade_coin.events.onInputDown.add(function() { this.upgrade("Coin Upgrade"); }, this);
+        upgrade_coin.visible = false;
+
         popUp = false;
     },
 
     display: function() {
         cadre.visible = true;
         cross.visible = true;
+        upgrade_coin.visible = true;
         popUp = true;
     },
 
@@ -50,6 +59,10 @@ Upgrade.prototype = {
         /* ARTHUR */
         var data = {user_id: user_id, building_id: building_id};
         socket.emit('AskCommerce', data);
+    },
+
+    upgrade: function(str) {
+        //console.log(str);
     }
 };
 
