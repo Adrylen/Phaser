dd4010c892866d09ba549164ee89cead0c13dc1e
Adrylen <adrien.martinez@isen-lille.fr>
2016-05-25 16:23:40 +0200

    Notifications (presque fini :!!!)

--- public/javascripts/game/window/rapport.js
+++ public/javascripts/game/window/rapport.js
@@ -1,29 +1,42 @@
 function createNotif(title, text, type, sendData = null) {
-	var data = {
-		res: 1,
-		from_user_id: sendData.from_user_id,
-		ask_for: sendData.ask_for,
-		to_user_id: sendData.to_user_id,
-		against: sendData.against
-	};
+	if(sendData != null)
+		var data = {
+			res: 1,
+			from_user_id: sendData.from_user_id,
+			ask_for: sendData.ask_for,
+			to_user_id: sendData.to_user_id,
+			against: sendData.against
+		};
+
 	var myStack = {"dir1":"down", "dir2":"right", "push":"top"};
 
 	var accept = {
 		title: "You've accepted the trading.", text: 'Resume',
 		type: 'info', icon: false, hide: true,
+		//addClass: "stack-custom", stack: myStack,
 		confirm: { confirm: false },
 		buttons: { closer: true, sticker: true }
 	};
 	var decline = {
-		title: "You've declined the trading.", text: '',
-		type: 'info', icon: false, hide: true,
-		confirm: { confirm: false },
-		buttons: { closer: true, sticker: true }
+		title: "You've declined the trading.",
+		text: '',
+		type: 'info',
+		icon: false,
+		hide: true,
+		//addClass: "stack-custom",
+		//stack: myStack,
+		confirm: {
+			confirm: false
+		},
+		buttons: {
+			closer: true,
+			sticker: true
+		}
 	};
 	if(type == 'alert')
 		var opts = {
 			title: title, text: text,
-			addClass: "stack-custom", stack: myStack,
+			//addClass: "stack-custom", stack: myStack,
 			icon: false, hide: false,
 			confirm: {
 				confirm: true,
@@ -41,24 +54,24 @@ function createNotif(title, text, type, sendData = null) {
 
 	if(type == 'confirm')
 		var opts = {
-			title: title, text: text,
-			addClass: "stack-custom", stack: myStack,
+			title: title, text: text, type: "notice",
+			//addClass: "stack-custom", stack: myStack,
 			icon: false, hide: false,
 			confirm: {
 				confirm: true,
 				buttons: [{
 					text: 'Accept',
 					click: function(notice) {
-						notice.update(accept);
 						data.res = 1;
 						socket.emit("ResCommerce", data);
+						notice.update(accept);
 					}
 				}, {
 					text: 'Decline',
 					click: function(notice) {
-						notice.update(decline);
 						data.res = 0;
 						socket.emit("ResCommerce", data);
+						notice.update(decline);
 					}
 				}]
 			},
